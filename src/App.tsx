5d6633dc2e65f92d1f4c5d50d34f234b51ee248b
JAYDEEP SAHU <jaydeepsahu971@gmail.com>
2026-06-04 18:26:26 +0530

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-20">
+
+      <h1 className='font-bold text-5xl'>Jaydeep</h1>
+      
+      <Button variant='contained'> Sahu </Button>
+      
     </div>
   );
 }
