cd6a07406f91eddd4b56162cf03fa8e9eac3819a
JAYDEEP SAHU <jaydeepsahu971@gmail.com>
2026-06-07 19:13:41 +0530

    commit 5

--- src/Customer/Components/CategorySheet.tsx
+++ src/Customer/Components/CategorySheet.tsx
@@ -9,30 +9,53 @@ import { electronicsLevelThree } from '../../Data/Category/LevelThree/Electronic
 import { homeKitchenLevelThree } from '../../Data/Category/LevelThree/Home&KitchenLevelThree'
 import { Box } from '@mui/material'
 
-const categoryTwo = {
+const categoryTwo:{[key:string]: any[]}={
   men: menLevelTwo,
   women: womenLevelTwo,
   home_kitchen: homeKitchenLevelTwo,
   electronics: electronicsLevelTwo
-
 }
 
-const categoryThree = {
+const categoryThree:{[key:string]: any[]} = {
   men: menLevelThree,
   women: womenLevelThree,
   electronics: electronicsLevelThree,
   home_kitchen: homeKitchenLevelThree
 }
 
-const CategorySheet = () => {
+const CategorySheet = ({ selectedCategory,setShowSheet}:any) => {
+
+
+  const childCategory = (category: any[], parentCategoryId: string) => {
+    return category.filter((child: any) => child.parentCategoryId === parentCategoryId)
+  }
+
   return (
-    <Box className="bg-white shadow-lg lg:h-[500px] overflow-y-auto">
-      <div className='flex text-sm flex-wrap'>
+    <Box
+      sx={{ zIndex: 2 }}
+      className="bg-white shadow-lg lg:h-[500px] overflow-y-auto"
+    >
+      <div className="flex text-sm flex-wrap">
+
         {
-          categoryTwo["men"]?.map((item) => <div>
-            <p className='text-primary-color mb-5 font-semibold'>{item.name}</p>
-          </div>)
-        }
+          categoryTwo[selectedCategory]?.map((item,index) =>
+            <div className={`p-8 lg:w-[20%] ${index % 2 === 0 ? "bg-slate-50" : "bg-white"}`}>
+
+                <p className="text-primary-color mb-5 font-semibold">{item.name}</p>
+                <ul className="space-y-3">
+                  {
+                    childCategory(categoryThree[selectedCategory], item.categoryId).map((child) => (
+                      <li
+                        key={child.categoryId}
+                        className="hover:text-primary-color cursor-pointer"
+                      >
+                        {child.name}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+        
+          )}
       </div>
     </Box>
   )
